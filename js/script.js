9c7117878b89452503f0493eac82847b1646f4e1
Radio Warning FM <warningfm@gmail.com>
2025-06-15 21:03:10 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ const url = 'https://api.zeno.fm/mounts/metadata/subscribe/skk0a2d6sd0uv';
 const API_KEY = "18fe07917957c289983464588aabddfb";
 
 // DEFAULT COVER
-const DEFAULT_COVER_ART = 'https://i.imgur.com/cbktQ2G.png';
+const DEFAULT_COVER_ART = 'https://cdn4.mbahnunungonline.net/img/cbktQ2G.png';
 
 // Variable to control history display: true = display / false = hides
 let showHistory = true; 
